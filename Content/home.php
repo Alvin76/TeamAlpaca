0bfdaa1286c14d7bdfcc0801e6250919bc95df62
Evan <ciriel@live.com>
2020-02-04 16:50:16 -0500

    Making a home page. Will look at php next

--- Content/home.php
+++ Content/home.php
@@ -3,34 +3,31 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome!</title>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   </head>
   <body>
-    <header>
-      <h1> TEAM ALPACA! </h1>
-    </header>
-    <nav>
 
-    </nav>
-    <main>
-      <?php nav_bar(); ?>
-      <h1>COBOL</h1>
-      <p>!--Note add about cobol here please--></p>
-      <p></p>
-      <h1>Who is team Alpaca?</h1>
-      <p>Adam, Taylor, Evan</p>
-      <!--we so totaly should put our old about us page here from team alpha :D -->
-    </main>
-    <footer>
-
-    </footer>
-
-
-
-    </body>
+    <div class="container-fluid home-main">
+    		<h1>Team Alpaca <span class="blinker">.</span></h1>
+    		<button type="button" class="btn btn-default">About Us</button>
+    	</div>
+    	<div class="container-fluid home-content1">
+    		<div class="row">
+    			<div class="col-md-6 content1-left">
+    				<h3>Why Use COBOL <span class="blinker">?</span></h3>
+    				<p>Take it away, Adam!</p>1
+    				<!-- <div class="content1-left"></div> -->
+    			</div>
+    			<div class="col-md-6 content1-right">
+    				<p>COBOL stands for COmmon Business Oriented Language. COBOL dates back to 1959 where it was made from the FLOW-MATIC. For more amazing history on COBOL you can find it here at https://en.wikipedia.org/wiki/COBOL</p>
+    			</div>
+    		</div>
+    	</div>
+    	<div class="container-fluid home-content2">
+    		<p><span>Footer</span> for all things <span>Alpaca</span>, including our <span>repositories</span> and <span>future projects</span>.</p>
+    	</div>
+      
   </body>
 </html>
