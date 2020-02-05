0863fc334c91d9d77933736d63b7afa4c436a5a6
Evan <ciriel@live.com>
2020-02-04 19:08:06 -0500

    Revert "umm"
    This reverts commit 312d87f5ad8ce26649b6ad7b63cd97b3cec43714.

--- includes/functions.php
+++ includes/functions.php
@@ -138,11 +138,11 @@ function page_content()
 {
 
     $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-    $path = getcwd() . '/' . config('content_path') . '/' . $page . '.php';
+    $path = getcwd() . '/' . config('Content_path') . '/' . $page . '.php';
     if (! file_exists($path)) {
-        $path = getcwd() . '/' . config('content_path') . '/404.php';
-}
-
+        $path = getcwd() . '/' . config('Content_path') . '/404.php';
+    }
+  echo ($path);
   echo file_get_contents($path);
 }
 
