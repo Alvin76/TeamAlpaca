705357511a22b12b0988ab2f42122de1a47e05ca
Evan <ciriel@live.com>
2020-02-13 18:26:20 -0500

    Removing

--- Content/home.php
+++ Content/home.php
@@ -6,7 +6,7 @@
     <title>Welcome!</title>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../template/home.css">
   </head>
   <body>
