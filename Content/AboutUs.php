1eb1e77770703a5648ac1fe69ece8c214a16d6e8
Evan <ciriel@live.com>
2020-02-13 19:00:39 -0500

    Try

--- Content/AboutUs.php
+++ Content/AboutUs.php
@@ -14,23 +14,23 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
       <a class="navbar-brand" href="#">
-      <img src="../Images/mystic.png" width="40" height="30" class="d-inline-block align-top" alt="">
-      Team Alpaca</a>
+        <img src="../Images/mystic.png" width="40" height="30" class="d-inline-block align-top" alt="">
+        Team Alpaca</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+        <span class="navbar-toggler-icon"></span>
+      </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home
-                  <span class="sr-only">(current)</span>
-                </a>
+              <span class="sr-only">(current)</span>
+            </a>
           </li>
           <li class="nav-item">
-            <a href="https://team-alpaca.herokuapp.com/Content/AboutUs.php">About</a>
+            <a class="nav-link" href="#">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Other</a>
+            <a class="nav-link" href="#">TN3270</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">COBOL</a>
@@ -63,7 +63,7 @@
           </div>
         </div>
         <!-- Slide Three - Set the background image for this slide in the line below -->
-        <div class="carousel-item" style="background-image: url('https://i.ytimg.com/vi/BZmXkpXm_sw/sddefault.jpg​')">
+        <div class="carousel-item" style="background-image: url('https://i.ytimg.com/vi/BZmXkpXm_sw/sddefault.jpg')">
           <div class="carousel-caption d-none d-md-block">
             <h3 class="display-4">COBOL</h3>
             <p class="lead">common business-oriented language</p>
@@ -71,15 +71,16 @@
         </div>
       </div>
       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
       <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
   </header>
+
 
     <!-- Page specific content will go down here. -->
     <div class="jumbotron jumbotron-fluid">
