d3976bf6548d6dedc5efbe0a5d6236eda73df81c
Evan <ciriel@live.com>
2020-02-18 01:38:18 -0500

    About test

--- Content/AboutUs.php
+++ Content/AboutUs.php
@@ -10,111 +10,162 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <?php head(); ?>
   <body>
-    <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">
-        <img src="../Images/mystic.png" width="40" height="30" class="d-inline-block align-top" alt="">
-        Team Alpaca</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a href="https://team-alpaca.herokuapp.com/Content/Home.php">Home
 
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="https://team-alpaca.herokuapp.com/Content/AboutUs.php">About</a>
-            <span class="sr-only">(current)</span>
-          </li>
-          <li class="nav-item">
-            <a href="https://team-alpaca.herokuapp.com/Content/HowToInstallIDE.php">TN3270</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">COBOL</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+        <!-- Navigation -->
+        <nav class="navbar navbar-dark primary-color">
+               <a class="navbar-brand" href="#">
+                <img src="../Images/mystic.png" width="40" height="30" class="d-inline-block align-top" alt="">
+                Team Alpaca</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a href="https://team-alpaca.herokuapp.com/Content/Home.php">Home
 
-  <header>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <!-- Slide One - Set the background image for this slide in the line below -->
-        <div class="carousel-item active" style="background-image: url('https://i.ytimg.com/vi/vLlJxw76qcs/maxresdefault.jpg')">
-          <div class="carousel-caption d-none d-md-block">
-            <h3 class="display-4">COBOL</h3>
-            <p class="lead">common business-oriented language</p>
-          </div>
-        </div>
-        <!-- Slide Two - Set the background image for this slide in the line below -->
-        <div class="carousel-item" style="background-image: url('https://image2.slideserve.com/3995624/cobol-structure-hello-world-l.jpg')">
-          <div class="carousel-caption d-none d-md-block">
-            <h3 class="display-4">COBOL</h3>
-            <p class="lead">common business-oriented language</p>
-          </div>
-        </div>
-        <!-- Slide Three - Set the background image for this slide in the line below -->
-        <div class="carousel-item" style="background-image: url('https://i.ytimg.com/vi/BZmXkpXm_sw/sddefault.jpg')">
-          <div class="carousel-caption d-none d-md-block">
-            <h3 class="display-4">COBOL</h3>
-            <p class="lead">common business-oriented language</p>
-          </div>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </header>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://team-alpaca.herokuapp.com/Content/AboutUs.php">About</a>
+                    <span class="sr-only">(current)</span>
+                  </li>
+                  <li class="nav-item">
+                    <a href="https://team-alpaca.herokuapp.com/Content/HowToInstallIDE.php">TN3270</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">COBOL</a>
+                  </li>
+                </ul>
+              </div>
+        </nav>
 
 
-    <!-- Page specific content will go down here. -->
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <main>
-          <div class="text-center">
-            <article>Welcome to our COBOL info website. </article>
-            <img src='../Images/alpaca-stare.jpg' alt='photo of me' />
-          </div>
-          <div class="container">
-      <h2> The Group <h2>
-      <img src="../Images/4alpacas.jpg" alt="Four Alpacas" class="img-fluid" id="4alpacas">
-    </div>
-    <div class="container">
-      <h2> Adam Davies <h2>
-      <img src="../Images/alpacaAdam.jpg" alt="alpacaAdam" class="img-fluid">
-    </div>
-    <div class="container">
-      <h2> Taylor Woods <h2>
-      <img src="../Images/alpacaTaylor.jpeg" alt="alpacaTaylor" class="img-fluid">
-    </div>
-    <div class="container">
-      <h2> Evan <h2>
-      <img src="../Images/alpacaEvan.jpeg" alt="alpacaEvan" class="img-fluid">
-    </div>
-    <!--
-    <div class="container">
-      <h2> James Ericsson <h2>
-      <img src="../Images/alpacaJames.jpeg" alt="alpacaJames" class="img-fluid">
-    </div> -->
-        </main>
-      </div>
-    </div>
+        <!-- Team -->
+    <section id="team" class="pb-5">
+        <div class="container">
+            <h5 class="section-title h1">OUR TEAM</h5>
+            <div class="row">
+                <!-- Team member -->
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="../Images/alpacaAdam.jpg" alt="alpacaAdam"></p>
+                                        <h4 class="card-title">Adam</h4>
+                                        <p class="card-text">Adam Davies</p>
+                                        <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title">Adam</h4>
+                                        <p class="card-text">Adam Description</p>
+                                        <ul class="list-inline">
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="https://github.com/Alvin76">
+                                                    <i class="fa fa-github" aria-hidden="true"></i>
+
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                    <i class="fa fa-google"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./Team member -->
+                <!-- Team member -->
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="src=../Images/alpacaEvan.jpeg" alt="alpacaEvan"></p>
+                                        <h4 class="card-title">Evan</h4>
+                                        <p class="card-text">Evan Jones</p>
+                                        <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title">Evan</h4>
+                                        <p class="card-text">Evan is lead UI Specialist of the team.</p>
+                                                                            <ul class="list-inline">
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="https://github.com/True-Final-Boss">
+                                                    <i class="fa fa-github" aria-hidden="true"></i>
+
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                    <i class="fa fa-google"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ./Team member -->
+                <!-- Team member -->
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="../Images/alpacaTaylor.jpeg" alt="alpacaTaylor"></p>
+                                        <h4 class="card-title">Taylor</h4>
+                                        <p class="card-text">Taylor Woods</p>
+                                        <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-plus"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title">Taylor</h4>
+                                        <p class="card-text">Taylor Description</p>
+                                        <ul class="list-inline">
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="https://github.com/Woods-Taylor">
+                                                    <i class="fa fa-github" aria-hidden="true"></i>
+
+                                                </a>
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <a class="social-icon text-xs-center" target="_blank" href="#">
+                                                    <i class="fa fa-google"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- ./Team member -->
+    </section>
+    <!-- Team -->
     <!-- Page specific content ends here. -->
     <?php footer(); ?>
   </body>
