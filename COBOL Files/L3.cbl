cece7595657fd32ac8d13d0787b1087107ea129d
Woods-Taylor <43648944+Woods-Taylor@users.noreply.github.com>
2020-03-02 15:42:10 -0500

    Update L3.cbl

--- COBOL Files/L3.cbl
+++ COBOL Files/L3.cbl
@@ -1,71 +1,63 @@
-000100 IDENTIFICATION DIVISION.                                         00010001
-000200 PROGRAM-ID.    CBLDB21.                                          00020002
-000300 AUTHOR.        CSU0127 ADAM DAVIES.                              00030037
-000400*INSTALLATION.  CSU.                                              00040004
-000500*SECURITY.      UNCLASSIFIED.                                     00050002
-000600*DATE-WRITTEN.  AUG 25, 2017.                                     00060017
-000700*DATE-COMPILED.                                                   00070002
-000710*****************************************************             00071041
-000720*PROGRAM IS TO PRINT A NEW LINE AFTER THE HELLO LINE*             00072041
-000730*****************************************************             00073041
-000800                                                                  00080002
-000900 ENVIRONMENT DIVISION.                                            00090002
-001000*                                                                 00100002
-001100 CONFIGURATION SECTION.                                           00110006
-001200 SOURCE-COMPUTER.                                                 00120015
-001300  6  Z13                                                          00130041
-001400     WITH DEBUGGING MODE                                          00140015
-001500     .                                                            00150015
-001600                                                                  00160015
-001700 OBJECT-COMPUTER.  IBM-Z13.                                       00170006
-001800*SPECIAL-NAMES.                                                   00180014
-001900*    CURRENCY SIGN IS EURO                                        00190014
-002000*    DECIMAL POINT IS COMMA.                                      00200014
-002100                                                                  00210007
-002200 INPUT-OUTPUT SECTION.                                            00220002
-002300*FILE-CONTROL.                                                    00230039
-002400*    SELECT PRINT-FILE         ASSIGN TO UT-S-REPORT.             00240039
-002500*                                                                 00250002
-002600 DATA DIVISION.                                                   00260002
-002700*                                                                 00270002
-002800*FILE SECTION.                                                    00280039
-002900*FD  PRINT-FILE                                                   00290039
-003000*    RECORDING MODE IS F                                          00300039
-003100*    LABEL RECORDS ARE OMITTED                                    00310039
-003200*    DATA RECORD IS PRINT-REC.                                    00320039
-003300*01  PRINT-REC.                                                   00330039
-003400*    05  FILLER                PIC X(120).                        00340039
-003500*                                                                 00350002
-003600 WORKING-STORAGE SECTION.                                         00360002
-003700                                                                  00370024
-003800 01  WS-COUNTERS-FLAGS.                                           00380024
-003900     10  EOF-FLAG              PIC X(03) VALUE "NO ".             00390027
-004000     10  WS-SPACING            PIC 9(01) VALUE 2.                 00400027
-004100     10  REC-COUNT             PIC 9(05) VALUE ZERO.              00410027
-004200     10  WS-LINE-COUNT         PIC 9(03) VALUE 0.                 00420027
-004300     10  WS-PAGE               PIC 9(03) VALUE 0.                 00430027
-004400                                                                  00440024
-004500*****************************************************             00450002
-004600*   STRUCTURE FOR PRESIDENT RECORD                  *             00460016
-004700*****************************************************             00470002
-004800*01  HELLOWORLD-RECORD.                                           00480037
-004900*    05  TEXT-HERE             PIC X(120).                        00490037
-005000                                                                  00500002
-005100 01  WS-PRINT-REC.                                                00510025
-005200     05  TEXT-HERE             PIC X(120).                        00520037
-005300                                                                  00530002
-005400 01  WS-DATE.                                                     00540024
-005500     10  YY                    PIC 9(02).                         00550027
-005600     10  MM                    PIC 9(02).                         00560027
-005700     10  DD                    PIC 9(02).                         00570027
-005800                                                                  00580024
-005900 01  WS-TIME.                                                     00590024
-006000     10  HH                    PIC 9(02).                         00600027
-006100     10  MN                    PIC 9(02).                         00610027
-006200     10  SS                    PIC 9(02).                         00620027
-006300     10  MS                    PIC 9(02).                         00630027
-006400                                                                  00640024
-006500 01  HEADING-1.                                                   00650024
+000100 IDENTIFICATION DIVISION.                                         00010009
+000200 PROGRAM-ID.    CBLDB21.                                          00020009
+000300 AUTHOR.        CSU0127 ADAM DAVIES.                              00030009
+000400*INSTALLATION.  CSU.                                              00040009
+000500*SECURITY.      UNCLASSIFIED.                                     00050009
+000600*DATE-WRITTEN.  FEB 23, 2020.                                     00060009
+000700*DATE-COMPILED.                                                   00070009
+000710*****************************************************             00071009
+000720*   PROGRAM IS ADD A MESSAGE EACH TIME YOU PRINT*             00072009
+000730*****************************************************             00073009
+000800                                                                  00080009
+000900 ENVIRONMENT DIVISION.                                            00090009
+001000*                                                                 00100009
+001100 CONFIGURATION SECTION.                                           00110009
+001200 SOURCE-COMPUTER.                                                 00120009
+001300     Z13                                                          00130009
+001400     WITH DEBUGGING MODE                                          00140009
+001500     .                                                            00150009
+001600                                                                  00160009
+001700 OBJECT-COMPUTER.  IBM-Z13.                                       00170009
+001800                                                                  00180009
+001900 INPUT-OUTPUT SECTION.                                            00190009
+002000 DATA DIVISION.                                                   00200009
+002100 WORKING-STORAGE SECTION.                                         00210009
+002200                                                                  00220009
+002300*****************************************************             00230009
+002400*   DATA VARS FOR PRINTING OUT AN MESSAGE           *             00240009
+002500*****************************************************             00250009
+002600                                                                  00260009
+002700 01  WS-PRINT-REC.                                                00270009
+002800     05  TEXT-HERE             PIC X(040).                        00280009
+002810     05  TEXT2-HERE            PIC X(040).                        00281009
+002820     05  TEXT3-HERE            PIC X(040).                        00282009
+002900                                                                  00290009
+003000 PROCEDURE DIVISION.                                              00300009
+003100                                                                  00310009
+003200*****************************************************             00320009
+003300*   MAIN PROGRAM ROUTINE                            *             00330009
+003400*****************************************************             00340009
+003500 010-PROG-START.                                                  00350009
+003600     PERFORM 300-PRINT-A-LINE                                     00360009
+003700     .                                                            00370009
+003800                                                                  00380009
+003801*****************************************************             00380109
+003802*   THIS IS TO MOVE DIFRENT VARS TO SAME OUTPUTLINE *             00380209
+003803*****************************************************             00380309
+003900 300-PRINT-A-LINE.                                                00390009
+004000     MOVE "HELLO WORLD" TO  TEXT-HERE                             00400009
+004100     PERFORM 700-PRINT-LINE                                       00410009
+004200     .                                                            00420009
+004300                                                                  00430009
+004400 700-PRINT-LINE.                                                  00440009
+004410     MOVE "WELCOME TO COBOL" TO TEXT2-HERE                        00441009
+004500     PERFORM 750-WRITE                                            00450009
+004600     .                                                            00460009
+004700                                                                  00470009
+004800 750-WRITE.                                                       00480009
+004900     DISPLAY WS-PRINT-REC                                         00490009
+005000     .                                                            00500009
+005100                                                                  00510009
 006600     10  FILLER                PIC X(02) VALUE SPACES.            00660027
 006700     10  H-DATE.                                                  00670024
 006800         15  H-MM              PIC 9(02).                         00680027
