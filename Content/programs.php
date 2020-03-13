31f0356729c6119f5771fdc18ab2200dcbb89efb
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2020-03-13 04:47:14 -0500

    hard 2

--- Content/programs.php
+++ Content/programs.php
@@ -46,7 +46,7 @@
 
 <!-- Page Heading -->
 <h1 class="my-4">TN3270
-  <small>Example Cobol Programs</small>
+  <small>What is the TN3270 vista and why do I need it?</small>
 </h1>
 
 <div class="row">
@@ -119,7 +119,7 @@
 </div>
 <div class="col-lg-6 mb-4">
     <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="https://team-alpaca.herokuapp.com/Images/hard1.PNG" alt=""></a>
+      <a href="#"><img class="card-img-top" src="https://team-alpaca.herokuapp.com/Images/Medium1.PNG" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
           <h3>Hard 1 If Statments and conditional use of functions</h3>
@@ -128,24 +128,14 @@
       </div>
     </div>
   </div>
-  div class="card h-100">
-      <a href="#"><img class="card-img-top" src="https://team-alpaca.herokuapp.com/Images/hard3.PNG" alt=""></a>
-      <div class="card-body">
-        <h4 class="card-title">
-          <h3>Hard 3 Creating the data division for H2</h3>
-        </h4>
-        <p class="card-text">The data division for the header is complex but necessary for the header to be used.</p>
-      </div>
-    </div>
-  </div>
   <div class="col-lg-6 mb-4">
     <div class="card h-100">
-      <a href="#"><img class="card-img-top" src="https://team-alpaca.herokuapp.com/Images/hard2.PNG" alt=""></a>
+      <a href="#"><img class="card-img-top" src="https://team-alpaca.herokuapp.com/Images/medium2.PNG" alt=""></a>
       <div class="card-body">
         <h4 class="card-title">
-          <h3>Hard 2 Making a Header with the Date</h3>
+          <h3>Medium 2 Concatenating Strings</h3>
         </h4>
-        <p class="card-text">This type of task is very common in Cobol since it is used largely for documents and reports.</p>
+        <p class="card-text">This shows how to combine strings together and display them.</p>
       </div>
     </div>
   </div>
